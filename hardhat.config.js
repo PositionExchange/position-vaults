5c63840a03d1cfefa90e064d1550fc752bb5e677
MusketGun <musket.gun@position.exchange>
2021-09-23 12:18:42 +0700

    refactor: add account testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,39 +6,38 @@ require('dotenv').config()
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.0",
-  defaultNetwork: "mainnet",
-  networks: {
-    localhost: {
-      url: "http://127.0.0.1:8545"
+    solidity: "0.8.0",
+    defaultNetwork: "mainnet",
+    networks: {
+        localhost: {
+            url: "http://127.0.0.1:8545"
+        },
+        hardhat: {},
+        testnet: {
+            url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+            chainId: 97,
+            gasPrice: 20000000000,
+            accounts: ['0999ffef3b3cf6dc1e85f4cae30edb83920b9c71e6d30c2d1b6345fc253a6795']
+        },
+        mainnet: {
+            url: "https://bsc-dataseed.binance.org/",
+            chainId: 56,
+            gasPrice: 20000000000,
+            // accounts: [process.env.PRIVATE_KEY]
+        }
     },
-    hardhat: {
+    etherscan: {
+        // Your API key for Etherscan
+        // Obtain one at https://etherscan.io/
+        apiKey: "RT1E1F2J16CTCCJZIZ5U8SFDACJ6VKF7U4"
     },
-    // testnet: {
-    //   url: "https://data-seed-prebsc-1-s1.binance.org:8545",
-    //   chainId: 97,
-    //   gasPrice: 20000000000,
-    //   accounts: {mnemonic: mnemonic}
-    // },
-    mainnet: {
-      url: "https://bsc-dataseed.binance.org/",
-      chainId: 56,
-      gasPrice: 20000000000,
-      // accounts: [process.env.PRIVATE_KEY]
+    paths: {
+        sources: "./contracts",
+        tests: "./test",
+        cache: "./cache",
+        artifacts: "./artifacts"
+    },
+    mocha: {
+        timeout: 20000
     }
-  },
-  etherscan: {
-    // Your API key for Etherscan
-    // Obtain one at https://etherscan.io/
-    apiKey: "RT1E1F2J16CTCCJZIZ5U8SFDACJ6VKF7U4"
-  },
-  paths: {
-    sources: "./contracts",
-    tests: "./test",
-    cache: "./cache",
-    artifacts: "./artifacts"
-  },
-  mocha: {
-    timeout: 20000
-  }
 };
